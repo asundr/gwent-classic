15b219ed8dbe410cbffdd29266c99850653ec524
Reynolds Costa <70243160+RandomPianist@users.noreply.github.com>
2022-06-22 15:58:00 -0300

    Add files via upload

--- gwent.js
+++ gwent.js
@@ -3283,9 +3283,9 @@ window.onload = function() {
 	playingOnline = window.location.href == "https://randompianist.github.io/gwent-classic-v2.0/";
 	document.getElementById("load_text").style.display = "none";
 	document.getElementById("button_start").style.display = "inline-block";
-	document.getElementById("deck-customization").style.display = "block";
-	document.getElementById("toggle-music").style.display = "block";
-	document.getElementsByTagName("main")[0].style.display = "block";
+	document.getElementById("deck-customization").style.display = "";
+	document.getElementById("toggle-music").style.display = "";
+	document.getElementsByTagName("main")[0].style.display = "";
 	document.getElementById("button_start").addEventListener("click", function() {
 		inicio();
 	});
